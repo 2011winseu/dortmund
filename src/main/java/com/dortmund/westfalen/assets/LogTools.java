b69e0a3eb7f9a5e98e1f38915cf8330f177d3951
mingwei.smw <mingwei.smw@alibaba-inc.com>
2016-05-21 16:22:50 +0800

    add assign logger for exception & erromsg

--- src/main/java/com/dortmund/westfalen/assets/LogTools.java
+++ src/main/java/com/dortmund/westfalen/assets/LogTools.java
@@ -7,15 +7,25 @@ import org.slf4j.LoggerFactory;
  * Created by mingwei.smw on 2016/4/21.
  */
 public class LogTools {
-    private static Logger logger = LoggerFactory.getLogger(LogTools.class);
+    private static Logger selfLog = LoggerFactory.getLogger(LogTools.class);
 
-    public static void logException( Exception e ){
+    public static void logException(Exception e) {
         StackTraceElement[] ele = new Throwable().getStackTrace();
-        logger.error("Clazz:"+ele[1].getClassName()+",Method:"+ele[1].getMethodName()+",Line number:"+ele[1].getLineNumber()+",Exception:", e );
+        selfLog.error("Clazz:" + ele[1].getClassName() + ",Method:" + ele[1].getMethodName() + ",Line number:" + ele[1].getLineNumber() + ",Exception:", e);
     }
 
-    public static void logError( String message ){
+    public static void logError(String message) {
         StackTraceElement[] ele = new Throwable().getStackTrace();
-        logger.error("Clazz:"+ele[1].getClassName()+",Method:"+ele[1].getMethodName()+",Line number:"+ele[1].getLineNumber()+",ErrorMessage:"+message );
+        selfLog.error("Clazz:" + ele[1].getClassName() + ",Method:" + ele[1].getMethodName() + ",Line number:" + ele[1].getLineNumber() + ",ErrorMessage:" + message);
+    }
+
+    public static void logException2AssignedFile(Logger assignedLog, Exception e) {
+        StackTraceElement[] ele = new Throwable().getStackTrace();
+        assignedLog.error("Clazz:" + ele[1].getClassName() + ",Method:" + ele[1].getMethodName() + ",Line number:" + ele[1].getLineNumber() + ",Exception:", e);
+    }
+
+    public static void logError2AssignedFile(Logger assignedLog, String message) {
+        StackTraceElement[] ele = new Throwable().getStackTrace();
+        assignedLog.error("Clazz:" + ele[1].getClassName() + ",Method:" + ele[1].getMethodName() + ",Line number:" + ele[1].getLineNumber() + ",ErrorMessage:" + message);
     }
 }
